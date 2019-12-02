6c5915354d79e69cd29cd7da5b4959689c91726f
JaredJose <42820125+JaredJose@users.noreply.github.com>
2019-12-02 12:57:25 -0800

    Added questions to switch case - questionMVC

--- AP_Exam/MVCQuestion.java
+++ AP_Exam/MVCQuestion.java
@@ -31,20 +31,22 @@ public class MVCQuestion extends Question{
 			this.choiceE = "";
 			break;
 		case 3:
-			this.question = "";
-			this.choiceA = "";
-			this.choiceB = "";
-			this.choiceC = "";
-			this.choiceD = "";
-			this.choiceE = "";
+			this.question = "What does the Model part of MVC control.";
+			this.choiceA = "The \"brain\" of the program, uses data + user input to complete purpose.";
+			this.choiceB = "The long-term stored information of a program. (Database)";
+			this.choiceC = "The GUI; how the user interacts with the program.";
+			this.choiceD = "The efficiency that the program runs at.";
+			this.choiceE = "All of the above.";
+			this.answerKey = 'B';
 			break;
 		case 4:
-			this.question = "";
-			this.choiceA = "";
-			this.choiceB = "";
-			this.choiceC = "";
-			this.choiceD = "";
-			this.choiceE = "";
+			this.question = "What is the purpose of MVC?";
+			this.choiceA = "Make code modular so you can modify parts without breaking the entirety of the program.";
+			this.choiceB = "Ensures that code is organized and purposeful.";
+			this.choiceC = "Ensures the proper breakdown of a problem.";
+			this.choiceD = "To make coding more confusing and difficult.";
+			this.choiceE = "A, B, C, but not D";
+			this.answerKey = 'E';
 			break;
 		default:
 			System.out.print("ERROR");
