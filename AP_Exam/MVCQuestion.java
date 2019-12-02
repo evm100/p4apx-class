94adf6605307dd4cca2ccea9d387dfc73df7c25d
JaredJose <42820125+JaredJose@users.noreply.github.com>
2019-12-02 12:48:00 -0800

    Setup the outline for MVCQuestion Class

--- AP_Exam/MVCQuestion.java
+++ AP_Exam/MVCQuestion.java
@@ -1,7 +1,55 @@
 package AP_Exam;
 
-public class MVCQuestion extends Question{
 
+public class MVCQuestion extends Question{
+	public void setupQuestion() {
+		int random = (int) Math.floor(Math.random()*5);
+		
+		switch(random){
+		case 0:
+			this.question = "";
+			this.choiceA = "";
+			this.choiceB = "";
+			this.choiceC = "";
+			this.choiceD = "";
+			this.choiceE = "";
+			break;
+		case 1:
+			this.question = "";
+			this.choiceA = "";
+			this.choiceB = "";
+			this.choiceC = "";
+			this.choiceD = "";
+			this.choiceE = "";
+			break;
+		case 2:
+			this.question = "";
+			this.choiceA = "";
+			this.choiceB = "";
+			this.choiceC = "";
+			this.choiceD = "";
+			this.choiceE = "";
+			break;
+		case 3:
+			this.question = "";
+			this.choiceA = "";
+			this.choiceB = "";
+			this.choiceC = "";
+			this.choiceD = "";
+			this.choiceE = "";
+			break;
+		case 4:
+			this.question = "";
+			this.choiceA = "";
+			this.choiceB = "";
+			this.choiceC = "";
+			this.choiceD = "";
+			this.choiceE = "";
+			break;
+		default:
+			System.out.print("ERROR");
+		}
+	}
 	
 	
 }
