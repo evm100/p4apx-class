17ef7caff0658064fdefe6cba2ad55e9e58fdab6
Edgar V <54725200+evm100@users.noreply.github.com>
2019-12-02 13:16:56 -0800

    hehexd

--- AP_Exam/ArrayListQuestions.java
+++ AP_Exam/ArrayListQuestions.java
@@ -1,26 +1,55 @@
 package AP_Exam;
-import java.util.ArrayList.*; 
+import java.util.ArrayList; 
 
-public class ArrayListQuestions extends Question 
+public class ArrayListQuestions extends Question
 {
+	public ArrayList<String> methods = new ArrayList<String>();
+	private int questionType;
 	
-	public ArrayListQuestions()
+	public ArrayListQuestions() 
 	{
+		/*
+		 * question
+		 * answer
+		 * choiceA
+		 * choiceBasd
+		 * choiceC
+		 * choiceD
+		 * choiceE
+		 */
 		
+		loadMethods();
+		questionType = (int) Math.floor(Math.random()*methods.size());
+		setupQuestion(questionType);
+	}
+	
+	private void loadMethods() 
+	{
+		methods.add("list.add(\"Ethan\");");
+		methods.add("list.remove(3);");
+		methods.add("list.contains(\"Ethan\");");
+		methods.add("list.set(3,\"Ethan\");");
+		methods.add("list.clone()");		
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void setupQuestion(int questionNum)
+	{
+		question = "What does the method " + methods.get(questionNum) + " do?";
+		
+		switch (questionNum)
+		{
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			break;
+		}
+	}
 }
